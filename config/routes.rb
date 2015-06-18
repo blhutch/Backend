c2680435296f122d74ae7c2a36ca6c013a604035
Henry Phan <hphan.cs@gmail.com>
2015-06-18 09:37:18 -0400

    Added comment header for Guesses routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+	# Guesses
   get 'posts/:post_id/guesses', to: 'guesses#index'                # Index of all guesses of a particular post
   get 'posts/:post_id/guesses/user/:username', to: 'guesses#user'  # Shows the guesses for a user in a particular post
   post 'posts/:post_id/guesses', to: 'guesses#create'              # Creates a guess for a specifc post
