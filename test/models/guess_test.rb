a2ce8a9af7d6ac1fe73366651d27ce21cd296672
Betty L. Hutchings <blhutch4@gmail.com>
2015-06-23 23:32:48 -0400

    testing

--- test/models/guess_test.rb
+++ test/models/guess_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class GuessTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'should not save guess without guess string' do
+    guess = Guess.new
+    assert_not guess.save
+  end
+
+  test 'should not save if guess string is longer than 10 characters' do
+    guess = Guess.new
+    guess.guess = 'this string is much longer than ten characters'
+    assert_not guess.save
+  end  
+
+  test 'should save if guess string is less than or equal to 10 characters' do
+    guess = Guess.new
+    guess.guess = 'test'
+    assert guess.save
+  end  
+
 end
